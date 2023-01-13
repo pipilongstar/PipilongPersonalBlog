a3f9833b18d6b4699e0a5fb82f0091dbb4774b06
ZSL <2534158602@qq.com>
2023-01-13 23:46:35 +0800

    first commit

---
+++ src/main/java/com/pipilong/PipilongPersonalBlogApplication.java
@@ -0,0 +1,13 @@
+package com.pipilong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipilongPersonalBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipilongPersonalBlogApplication.class, args);
+    }
+
+}
